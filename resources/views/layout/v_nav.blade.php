c3ebde2d4c3d9b26fdd8a1192dca944d4dce51aa
mramadhanurh <mramadhanur@gmail.com>
2024-01-23 09:08:11 +0700

    add auth & dashboard admin

---
+++ resources/views/layout/v_nav.blade.php
@@ -0,0 +1,37 @@
+<ul class="menu-inner py-1">
+    <li class="menu-item active open">
+        <a href="/admin/home" class="menu-link">
+            <i class="menu-icon tf-icons bx bx-home-circle"></i>
+            <div data-i18n="Basic">Dashboard</div>
+        </a>
+    </li>
+
+    <li class="menu-item">
+        <a href="cards-basic.html" class="menu-link">
+            <i class="menu-icon tf-icons bx bx-home-circle"></i>
+            <div data-i18n="Basic">Cards</div>
+        </a>
+    </li>
+
+    <li class="menu-item">
+        <a href="cards-basic.html" class="menu-link">
+            <i class="menu-icon tf-icons bx bx-home-circle"></i>
+            <div data-i18n="Basic">Cards</div>
+        </a>
+    </li>
+
+    <li class="menu-item">
+        <a href="cards-basic.html" class="menu-link">
+            <i class="menu-icon tf-icons bx bx-home-circle"></i>
+            <div data-i18n="Basic">Cards</div>
+        </a>
+    </li>
+
+    <li class="menu-item">
+        <a href="cards-basic.html" class="menu-link">
+            <i class="menu-icon tf-icons bx bx-home-circle"></i>
+            <div data-i18n="Basic">Cards</div>
+        </a>
+    </li>
+
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,11 +14,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+// Route::view('/admin/home', 'admin.dashboard');
+Route::get('/admin/home', [AdminController::class, 'index']);
+
 Route::get('/exams', [QuestionController::class, 'index']);
 Route::get('/get-questions', [QuestionController::class, 'getQuestions']);
 Route::get('/question-by-id/{id}', [QuestionController::class, 'getQuestionById']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
